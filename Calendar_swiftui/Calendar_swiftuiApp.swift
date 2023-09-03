3cfb0dbfab23b32d63cf29d837bd30cef5de5f64
clf94-dev <clf94@hotmail.es>
2023-09-03 18:00:27 +0200

    Initial Commit

---
+++ Calendar_swiftui/Calendar_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar_swiftuiApp.swift
+//  Calendar_swiftui
+//
+//  Created by Carmen Lucas on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Calendar_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calendar_swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calendar_swiftui
+//
+//  Created by Carmen Lucas on 3/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
