f3680638ced50d6f7b6946defea48aa134defde4
clf94-dev <clf94@hotmail.es>
2023-09-03 18:24:24 +0200

    - add base navigation calendar

--- Calendar_swiftui/ContentView.swift
+++ Calendar_swiftui/ContentView.swift
@@ -10,12 +10,53 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack{
+                Circle()
+                    .foregroundStyle(.gray)
+                    .opacity(0.5)
+                Image(systemName: "calendar")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50)
+            }.frame(width: 110)
+            Text("Calendly course")
+                .font(.title)
+                .bold()
+            
+            Rectangle()
+                .frame(height:1)
+            VStack{
+                Text("Select a Day")
+                    .font(.title2)
+                    .bold()
+                
+                HStack{
+                    Spacer()
+                    Button{
+                            print("backwards")
+                    } label:{
+                        Image(systemName: "lessthan")
+                            .resizable()
+                            .scaledToFill()
+                        .frame(width: 16, height: 28)
+                    }
+                    Spacer()
+                    Text("September 2023")
+                    Spacer()
+                    Button{
+                        
+                    } label: {
+                        Image(systemName: "greaterthan")
+                            .resizable()
+                            .scaledToFill()
+                        .frame(width: 16, height: 28)
+                    }
+                    Spacer()
+                }
+            }
+           
         }
-        .padding()
+        
     }
 }
 
